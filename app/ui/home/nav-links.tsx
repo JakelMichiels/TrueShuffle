3ea4905d02563cd920c696a042496a8ee14f3bf6
Jake Logan Michiels <jakelmichiels@gmail.com>
2024-08-28 17:54:54 -0400

    added sidebar

---
+++ app/ui/home/nav-links.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { HomeIcon, FireIcon } from "@heroicons/react/24/outline";
+import { ShuffleIcon, LooperIcon } from "@/app/ui/icons";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: "Home", href: "/home", icon: HomeIcon },
+  {
+    name: "TrueShuffle",
+    href: "/home/trueshuffle",
+    icon: ShuffleIcon,
+  },
+  { name: "SongSwipe", href: "/home/songswipe", icon: FireIcon },
+  { name: "Looper", href: "/home/looper", icon: LooperIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-400 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-white-600": pathname === link.href,
+              },
+              {"text-black": pathname !== link.href }
+
+            )}
+          >
+            <LinkIcon className="w-6 stroke-1 " />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
